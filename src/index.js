c719c1f5878a66bbb7c3ea59264bf93cd461efcb
Sedv1k <dimdimdim5561@gmail.com>
2022-02-24 18:07:05 +0200

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,13 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodeMorse = "";
+    let empty = " ";
+    for (i = 0; i < expr.length; i += 10) {
+        let character = expr.slice(i, i + 10).replace(/00/g, "").replace(/10/g, ".").replace(/11/g, "-");
+        decodeMorse += MORSE_TABLE[character] || empty;
+    }
+    return decodeMorse;
 }
 
 module.exports = {
